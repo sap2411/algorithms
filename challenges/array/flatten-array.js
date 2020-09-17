d5f737ca62a08cdd05116d5ac4874871fbbbdda4
sap2411 <steven.parsons.us@gmail.com>
2020-09-17 10:57:05 -0700

    adds flatten algo

---
+++ challenges/array/flatten-array.js
@@ -0,0 +1,15 @@
+return a flattened array
+
+//[1,[[2]],3,[4,[5]]] => [1,2,3,4,5]
+
+function flatten(oldArr){
+    var newArr = []
+        for(var i = 0; i < oldArr.length; i++){
+          if(Array.isArray(oldArr[i])){
+                newArr = newArr.concat(flatten(oldArr[i]))
+          } else {
+                newArr.push(oldArr[i])
+          }
+    } 
+    return newArr;
+  }
